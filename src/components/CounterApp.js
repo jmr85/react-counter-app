b6c5d4db87d2b6b1258949784c1ecab5348d03ed
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2021-08-07 00:44:39 -0300

    first commit

---
+++ src/components/CounterApp.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CounterApp = ({value}) => {
+
+    return (
+        <div>
+            <h1>CounterApp</h1>
+            <h2> { value } </h2>
+        </div>
+    )
+    
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number.isRequired
+}
+
+export default CounterApp;
